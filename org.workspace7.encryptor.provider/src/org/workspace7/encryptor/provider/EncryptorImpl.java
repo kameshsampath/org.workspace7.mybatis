e14e274c89cd261135d6ab292f881857ccefc6cf
Kamesh Sampath <kamesh.sampath@hotmail.com>
2016-06-07 21:23:21 +0530

    Added back eclipse project files

--- org.workspace7.encryptor.provider/src/org/workspace7/encryptor/provider/EncryptorImpl.java
+++ org.workspace7.encryptor.provider/src/org/workspace7/encryptor/provider/EncryptorImpl.java
@@ -1,31 +1,11 @@
 package org.workspace7.encryptor.provider;
 
-import org.jasypt.encryption.StringEncryptor;
-import org.osgi.framework.BundleContext;
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 
 /**
  * 
  */
-@Component(name="org.workspace7.encryptor",service=StringEncryptor.class,immediate=true)
-public class EncryptorImpl implements StringEncryptor {
-
-	
-	@Activate
-	public void activated(BundleContext bundleContext){
-		System.out.println("EncryptorImpl.activated()");
-	}
-	
-	//TODO:FOR now just return the same string, later we can add the properties via config admin
-	@Override
-	public String decrypt(String encryptedString) {
-		return encryptedString;
-	}
-
-	@Override
-	public String encrypt(String unenryptedString) {
-		return unenryptedString;
-	}
+@Component(name = "org.workspace7.encryptor")
+public class EncryptorImpl {
 
 }
